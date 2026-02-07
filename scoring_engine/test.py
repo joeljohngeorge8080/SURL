ec16fde0a5f36e5b4bd21e81bf078072b2582543
Joel John George <joeljohngeorge2005@gmail.com>
2026-02-07 15:06:49 +0530

    Added the test scoring for the staic analysis , it is inside the test folder

---
+++ scoring_engine/test.py
@@ -0,0 +1,10 @@
+from static_analysis.static_runner import run_static_analysis
+from scoring_engine.score_calculator import calculate_risk_score
+
+url = input("Enter URL: ")
+static_results = run_static_analysis(url)
+
+final_result = calculate_risk_score(static_results)
+
+print("\n=== FINAL RISK REPORT ===")
+print(final_result)
