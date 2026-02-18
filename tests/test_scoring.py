2bcf380b433929c0e6b623b6c54b2702ccb0b7f8
Joel John George <joeljohngeorge2005@gmail.com>
2026-02-18 17:36:02 +0530

    Added the test_scoring.py for a trial in pbh work

--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -1,10 +1,17 @@
 from static_analysis.static_runner import run_static_analysis
-from scoring_engine.score_calculator import calculate_risk_score
+from scoring_engine.pbh_fingerprint import generate_pbh_fingerprint
 
-url = input("Enter URL: ")
-static_results = run_static_analysis(url)
 
-final_result = calculate_risk_score(static_results)
+def main():
+    url = input("Enter URL: ")
 
-print("\n=== FINAL RISK REPORT ===")
-print(final_result)
+    static_results = run_static_analysis(url)
+
+    pbh = generate_pbh_fingerprint(static_results)
+
+    print("\n=== PBH RESULT ===")
+    print(pbh)
+
+
+if __name__ == "__main__":
+    main()
